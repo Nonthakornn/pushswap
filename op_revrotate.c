96276fbf5acd96ea767508f9388b556520c4b244
Nonthakorn <nonthakorn.che@gmail.com>
2024-12-21 14:26:11 +0700

    fix bug on reverse_rotate

--- op_revrotate.c
+++ op_revrotate.c
@@ -5,27 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nchencha <nchencha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/13 15:14:51 by nchencha          #+#    #+#             */
-/*   Updated: 2024/12/16 19:25:08 by nchencha         ###   ########.fr       */
+/*   Created: 2024/12/21 14:20:45 by nchencha          #+#    #+#             */
+/*   Updated: 2024/12/21 14:23:57 by nchencha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	rev_rotate(t_stack **head)
+static void	rev_rotate(t_stack **stack)
 {
-	t_stack	*last_node;
-	t_stack	*before_last_node;
+	t_stack	*last;
+	t_stack	*temp;
 
-	if (!(*head) || !(*head)->next)
+	if (!stack || !*stack || !(*stack)->next)
 		return ;
-	last_node = ft_lstlast(*head);
-	before_last_node = last_node->previous;
-	before_last_node->next = NULL;
-	last_node->previous = NULL;
-	last_node->next = *head;
-	(*head)->previous = last_node;
-	*head = last_node;
+	last = *stack;
+	while (last->next)
+	{
+		temp = last;
+		last = last->next;
+	}
+	temp->next = NULL;
+	last->next = *stack;
+	last->previous = NULL;
+	(*stack)->previous = last;
+	*stack = last;
+	temp = *stack;
+	while (temp->next)
+	{
+		temp->next->previous = temp;
+		temp = temp->next;
+	}
 }
 
 void	rra(t_stack **stack_a)
