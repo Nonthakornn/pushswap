abc856d0b4808a662af5a19ec424aef112a13106
Nonthakorn <nonthakorn.che@gmail.com>
2024-12-21 11:35:33 +0700

    add utils2

---
+++ utils2.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchencha <nchencha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 18:31:28 by nchencha          #+#    #+#             */
+/*   Updated: 2024/12/21 11:35:14 by nchencha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*find_biggest_node(t_stack *stack)
+{
+	long	max;
+	t_stack	*biggest_node;
+
+	if (!stack)
+		return (NULL);
+	max = INT_MIN;
+	while (stack)
+	{
+		if (stack->nbr > max)
+		{
+			max = stack->nbr;
+			biggest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (biggest_node);
+}
+
+t_stack	*find_smallest_node(t_stack *stack)
+{
+	long	min;
+	t_stack	*smallest_node;
+
+	if (!stack)
+		return (NULL);
+	min = INT_MAX;
+	while (stack)
+	{
+		if (stack->nbr < min)
+		{
+			min = stack->nbr;
+			smallest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (smallest_node);
+}
+
+t_stack	*find_cheapest(t_stack *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack)
+	{
+		if (stack->cheapest)
+			return (stack);
+		stack = stack->next;
+	}
+	return (NULL);
+}
